9a3e6c6636b7e5682aa251e8de1d66fd09edc180
Steve Pentler <stevepentler@gmail.com>
2016-01-04 09:46:45 -0700

    randomized sizes for rotations

---
+++ p5-release/sketch01.js/sketch.js
@@ -0,0 +1,154 @@
+// function setup() {
+//   width = 1000;
+//   height = 400;
+//   createCanvas(width, height);
+//   background(0);
+//   size = 40;
+//   size_two = 40;
+//   color = 255;
+//   count = 0;
+
+// }
+
+// function draw() {
+//   fill(random(color), random(color), random(color));
+//   ellipse(random(width), random(height), size, size_two);
+// }
+ // ----------------------------------------
+
+// function setup() {
+//   createCanvas(1280, 680);
+//   background('black');
+// }
+
+// function draw() {
+//   if (mouseIsPressed) {
+//     fill('magenta');
+//     ellipse(mouseX, mouseY, 30, 30);
+//   }
+//   else {
+//     fill(255);
+//     ellipse(mouseX, mouseY, 20, 20);
+//   }
+// }
+// -------------------------------------------
+//  Crazy random colors, moves to the right 
+// function setup() {
+//   createCanvas(1280, 680);
+//   background('black');
+// }
+
+// function draw() {
+//   r = random(255)
+//   g = random(255)
+//   b = random(255)
+//   if (mouseIsPressed) {
+//     fill('magenta');
+//     ellipse(mouseX, mouseY, 30, 30);
+//   }
+//   else {
+//     fill(r, g, b);
+//     ellipse(mouseX, mouseY, 40, 20);
+//     mouseX+=5;
+//   }
+// }
+// ---------------------------------------------
+
+// // Crazy rotating cylinder thing 
+// function setup(){
+//   createCanvas(1200, 800, WEBGL);
+// }
+
+// function draw(){
+//   background('black');
+//   // rotateX(frameCount * 0.01);
+//   rotateZ(frameCount * 0.01);
+//   cone(200, 300);
+//   torus(100, 60);
+//   cylinder(150, 20);
+// }
+
+
+// -----------------------------------
+// //  Wicked XY rotation
+function setup(){
+  createCanvas(1200, 800, WEBGL);
+}
+
+function draw(){
+  background('black');
+  rotateX(frameCount * random(0.01));
+  rotateZ(frameCount * random(0.9));
+  cone(random(200), random(300));
+  torus(100, 60);
+  cylinder(random(150), random(20));
+  ellipsoid(random(100), random(100), random(50));
+}
+
+// --------------------------------------
+// function preload() {
+//   mySound = loadSound('/assets/Get Lucky (Vanderway Edit).mp3');
+// }
+
+// function setup() {
+//   createCanvas(100, 100);
+//   background(0, 255, 0);
+  
+//   textAlign(CENTER);
+//   text('click here to play', width/2, height/2);
+
+//   mySound.setVolume(0.9);
+// }
+
+// // play sound on mouse press over canvas
+// function mousePressed() {
+//   if (mouseX < width && mouseY < height && mouseX > 0 && mouseY > 0) {
+//     mySound.play(song);
+//   }
+// }
+
+// --------------------------------------
+// var song;
+
+// function setup() {
+//   song = loadSound('assets/Get Lucky (Vanderway Edit).mp3');
+//   createCanvas(720, 200);
+//   background(255,0,0);
+// }
+
+// function mousePressed() {
+//   if (song.isPlaying() ) { // .isPlaying() returns a boolean
+//     song.stop();
+//     background(255,0,0);
+//   } else {
+//     song.play();
+//     background(0,255,0);
+//   }
+// }
+
+// ----------------------------------------
+// function preload(){
+//   sound = loadSound('assets/GotLucky.mp3');
+// }
+// function setup() {
+//   cnv = createCanvas(100,100);
+//   sound.amp(0.6);
+//   amp = new p5.Amplitude();
+// }
+// function draw() {
+//   background(0);
+//   fill(255);
+//   var level = amplitude.getLevel();
+//   var size = map(level, 0, 1, 0, 200);
+//   ellipse(width/2, height/2, size, size);
+// }
+
+// function mousePressed() {
+//  if (song.isPlaying() ) {
+//    song.pause();
+//  } else {
+//    song.play();
+//  }
+// }
+
+
